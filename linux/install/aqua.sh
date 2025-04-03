fc1bf12cc189aa083f6ba49d88fe097b451cf1c3
Alberto Ponces <ponces26@gmail.com>
2025-04-03 16:26:45 +0100

    feat(linux): add `aqua` install script

---
+++ linux/install/aqua.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+curl -sfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.1.1/aqua-installer | bash
+
+mkdir -p $HOME/.config/aquaproj-aqua
+echo 'export AQUA_ROOT_DIR="$HOME/.local/share/aquaproj-aqua"' >> $HOME/.bashrc
+echo 'export AQUA_CONFIG_DIR="$HOME/.config/aquaproj-aqua"' >> $HOME/.bashrc
+echo 'export AQUA_GLOBAL_CONFIG="$AQUA_CONFIG_DIR/aqua.yaml"' >> $HOME/.bashrc
+echo 'export PATH=$AQUA_ROOT_DIR/bin:$PATH' >> $HOME/.bashrc
