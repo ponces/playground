7a52dbf5922fd9f98f0e0ab8d8fd1d61bb6753a1
Alberto Ponces <ponces26@gmail.com>
2025-04-04 00:07:11 +0100

    feat(apkmerge): improve error handling

--- linux/run/apkmerge.sh
+++ linux/run/apkmerge.sh
@@ -7,10 +7,17 @@ set -e
 curl -sfL https://go.ponces.xyz/apkmd | bash -s -- "$1" "$2"
 downFile="$(ls -Art | tail -n 1)"
 
-curl -sfL https://api.github.com/repos/REAndroid/APKEditor/releases | \
-        jq -r '.[0].assets[] | .browser_download_url | select(endswith(".jar"))' | \
-        wget -q -i - -O $TMPDIR/apkeditor.jar
-java -jar $TMPDIR/apkeditor.jar m -i "$downFile"
+if [[ "$downFile" == *".apk" ]]; then
+    exit 0
+elif [[ "$downFile" == *".apkm" ]]; then
+        curl -sfL https://api.github.com/repos/REAndroid/APKEditor/releases | \
+                jq -r '.[0].assets[] | .browser_download_url | select(endswith(".jar"))' | \
+                wget -q -i - -O $TMPDIR/apkeditor.jar
+        java -jar $TMPDIR/apkeditor.jar m -i "$downFile"
 
-curl -sfL https://go.ponces.xyz/keystore -o $TMPDIR/debug.keystore
-apksigner sign --ks $TMPDIR/debug.keystore --ks-pass pass:android "$downFile"
+        curl -sfL https://go.ponces.xyz/keystore -o $TMPDIR/debug.keystore
+        apksigner sign --ks $TMPDIR/debug.keystore --ks-pass pass:android "$downFile"
+else
+    echo "No valid APK file found."
+    exit 1
+fi
