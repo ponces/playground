e27423a16d0fb68dfad2721688793109bc026d26
Alberto Ponces <ponces26@gmail.com>
2025-04-10 15:09:19 +0100

    chore(install): improve `k3s` install script to install an user owned config

--- linux/install/k8s.sh
+++ linux/install/k8s.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-curl -sfSL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable traefik" bash -s -
+[ "$(id -u)" -ne 0 ] && SUDO="sudo" || SUDO=""
+
+curl -sfSL https://get.k3s.io | bash -s -
+
+mkdir -p $HOME/.kube/config
+$SUDO cp -f /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+$SUDO chown $USER:$USER $HOME/.kube/config
+chmod 600 $HOME/.kube/config
 
 curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/v1.16.0/deploy/install-driver.sh | bash -s "v1.16.0" --
 kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.0/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
