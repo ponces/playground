268c9859b66c8f7b779147dabc1a77674e6eb0d4
Alberto Ponces <ponces26@gmail.com>
2025-06-25 00:26:32 +0100

    fix(termux): update `zsh` install

--- linux/install/zsh.sh
+++ linux/install/zsh.sh
@@ -12,17 +12,15 @@ if ! command -v zsh >/dev/null; then
     $SUDO apt-get install -y zsh
 fi
 
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yes | sh -c "$(curl -sfSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/# export PATH/export PATH/' $HOME/.zshrc
 
 git clone -q --depth=1 https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone -q --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-if [ -f /etc/pam.d/chsh ]; then
-    $SUDO sed 's/required/sufficient/g' -i /etc/pam.d/chsh
-fi
-
 if [ ! -z "$TERMUX_VERSION" ]; then
-    $SUDO chsh -s zsh $(whoami)
+    chsh -s zsh $(whoami)
 else
+    $SUDO sed -i 's/required/sufficient/g' /etc/pam.d/chsh || true
     $SUDO chsh -s $(which zsh) $(whoami)
 fi
