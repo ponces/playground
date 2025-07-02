1ed5ed284efd2327466edbf5db7e9533a5d8e878
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-07-02 11:47:33 +0100

    feat(run): add aconfig flags update script

---
+++ android/run/aconfig.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+[ -z "$TMPDIR" ] && [ -d /tmp ] && TMPDIR="/tmp"
+
+url="https://github.com/$1/$2/archive/refs/heads/$3.zip"
+if [ -z "$3" ]; then
+    url="https://api.github.com/repos/$1/$2/zipball"
+fi
+
+rm -rf "$TMPDIR"/repo.zip
+rm -rf "$TMPDIR"/repo
+curl -sfSL "$url" -o "$TMPDIR"/repo.zip
+unzip -q "$TMPDIR"/repo.zip -d "$TMPDIR"/repo
+
+pushd "$TMPDIR"/repo/*/release/aconfig/* >/dev/null
+for dir in */; do
+    if [ -d "$dir" ]; then
+        pushd "$dir" >/dev/null
+        sed -i 's/*.textproto/*_flag_values.textproto/g' Android.bp
+        sed -i 's/aconfig-values-/aconfig-values-platform_build_release-/g' Android.bp
+        sed -i 's/platform_build_release-platform_build_release/platform_build_release/g' Android.bp
+        for file in *.textproto; do
+            if [[ "$file" == *"_flag_values.textproto" ]]; then
+                continue
+            fi
+            mv "$file" "${file%.textproto}_flag_values.textproto"
+        done
+        popd >/dev/null
+    fi
+done
+popd >/dev/null
+
+if [ -d aconfig ]; then
+    cp -r "$TMPDIR"/repo/*/release/aconfig/* ./aconfig/
+elif [ -d release ]; then
+    cp -r "$TMPDIR"/repo/*/release/* ./release/
+else
+    cp -r "$TMPDIR"/repo/*/release ./release
+fi
