a15c4e76ae253a43a354701fe6e7059d9540d5d3
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-07-04 14:44:35 +0100

    feat(install): add `rbw` install script

---
+++ linux/install/rbw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+[ -z "$TMPDIR" ] && [ -d /tmp ] && TMPDIR="/tmp"
+[ "$(uname -m)" = "aarch64" ] && ARCH="arm64" || ARCH="amd64"
+[ ! -z "$TERMUX_VERSION" ] && OS="linux-android" || OS="linux"
+
+link=$(curl -sfSL "https://api.github.com/repos/ponces/rbw/releases/latest" | \
+            jq -r ".assets[] | \
+                select(.name | endswith(\"_${OS}_${ARCH}.tar.gz\")) | \
+                .browser_download_url" | \
+            head -1)
+curl -sfSL "$link" -o $TMPDIR/rbw.tar.gz
+tar -xzf $TMPDIR/rbw.tar.gz -C $HOME/.local/bin
+rm -f $TMPDIR/rbw.tar.gz
+rm -rf $HOME/.local/bin/completion
