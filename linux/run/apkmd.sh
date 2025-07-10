c62b5ec8ad936328b5d12f7dace5b316d20f1782
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-07-10 23:40:58 +0100

    refactor(run): update `apkmd` script

--- linux/run/apkmd.sh
+++ linux/run/apkmd.sh
@@ -13,15 +13,20 @@ curl -sfSL https://github.com/tanishqmanuja/apkmirror-downloader/releases/latest
 chmod +x $TMPDIR/apkmd
 
 if [ ! -z "$3" ]; then
-    additional="--version $3"
+    version="$3"
+else
+    version="stable"
 fi
 
-eval $TMPDIR/apkmd download "$1" "$2" "$additional" --arch "arm64-v8a" 2>&1 | tee $TMPDIR/apkmd.log
+eval $TMPDIR/apkmd download "$1" "$2" --version "$version" --arch "arm64-v8a" --type "apk" --dpi "nodpi" 2>&1 | tee $TMPDIR/apkmd.log
 if grep -q -e Unable -e Error $TMPDIR/apkmd.log; then
-    eval $TMPDIR/apkmd download "$1" "$2" "$additional" --arch "universal" 2>&1 | tee $TMPDIR/apkmd.log
+    eval $TMPDIR/apkmd download "$1" "$2" --version "$version" --arch "universal" --type "apk" --dpi "nodpi" 2>&1 | tee $TMPDIR/apkmd.log
 fi
 if grep -q -e Unable -e Error $TMPDIR/apkmd.log; then
-    eval $TMPDIR/apkmd download "$1" "$2" "$additional" --arch "universal" --type "bundle" --dpi "any"
+    eval $TMPDIR/apkmd download "$1" "$2" --version "$version" --arch "arm64-v8a + armeabi-v7a" --type "bundle" --dpi "nodpi" 2>&1 | tee $TMPDIR/apkmd.log
+fi
+if grep -q -e Unable -e Error $TMPDIR/apkmd.log; then
+    eval $TMPDIR/apkmd download "$1" "$2" --version "$version" --arch "universal" --type "bundle" --dpi "any"
 fi
 
 rm -f $TMPDIR/apkmd
