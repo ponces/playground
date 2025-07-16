c9a00d31eba6331c207d0399e0ac1b4f9d7cd0f9
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-07-16 21:49:44 +0100

    feat(install): add suport for rust install on `termux`

--- linux/install/rust.sh
+++ linux/install/rust.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-curl -sfSL https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-rustup default stable
-rustup target add aarch64-linux-android
-cargo install cargo-ndk
+if [ ! -z "$TERMUX_VERSION" ]; then
+    pkg install rust
+else
+    curl -sfSL https://sh.rustup.rs | sh -s -- -y
+    . "$HOME/.cargo/env"
+    rustup default stable
+    rustup target add aarch64-linux-android
+    cargo install cargo-ndk
+fi
