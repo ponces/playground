d380fff19317bcb646bef4a1817353735e51fb46
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-08-19 17:07:04 +0100

    feat(install): add `termv` install script

---
+++ linux/run/iptv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+[ -z "$TMPDIR" ] && [ -d /tmp ] && TMPDIR="/tmp"
+[ "$(id -u)" -ne 0 ] && SUDO="sudo" || SUDO=""
+
+$SUDO apt-get update
+$SUDO apt-get install -y curl fzf gawk jq mpv xdo
+
+curl -sfSL https://raw.githubusercontent.com/ponces/termv/main/termv -o $HOME/.local/bin/termv
+chmod +x $HOME/.local/bin/termv
+
+if [ ! -z "$TERMUX_VERSION" ]; then
+    link=$(curl -sfSL "https://api.github.com/repos/mpv-android/mpv-android/releases/latest" | \
+            jq -r ".assets[] | \
+                select(.name | endswith(\"default-universal-release.apk\")) | \
+                .browser_download_url" | \
+            head -1)
+    curl -sfSL "$link" -o $TMPDIR/mpv.apk
+    termux-open $TMPDIR/mpv.apk
+fi
