19a8c2a631965eb373bd3c15636c91ce906bcd77
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-09-08 13:50:48 +0100

    refactor(install): update `lazydocker` and `k9s` install scripts

--- linux/install/k9s.sh
+++ linux/install/k9s.sh
@@ -6,8 +6,8 @@ set -e
 [ "$(uname -m)" = "aarch64" ] && ARCH="arm64" || ARCH="amd64"
 
 curl -sfSL https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_$ARCH.tar.gz -o $TMPDIR/k9s.tar.gz
-tar -zxf $TMPDIR/k9s.tar.gz k9s
+tar -xzf $TMPDIR/k9s.tar.gz k9s
 rm -f $TMPDIR/k9s.tar.gz
 
-cp -f k9s $HOME/.local/bin/k9s
+mv k9s $HOME/.local/bin/k9s
 chmod +x $HOME/.local/bin/k9s
--- linux/install/lazydocker.sh
+++ linux/install/lazydocker.sh
@@ -2,4 +2,17 @@
 
 set -e
 
-curl -sfSL https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+[ -z "$TMPDIR" ] && [ -d /tmp ] && TMPDIR="/tmp"
+[ "$(uname -m)" = "aarch64" ] && ARCH="arm64" || ARCH="x86_64"
+
+url=$(curl -sfSL "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | \
+            jq -r ".assets[] | \
+                select(.name | endswith(\"_Linux_$ARCH.tar.gz\")) | \
+                .browser_download_url" | \
+            head -1)
+curl -sfSL "$url" -o $TMPDIR/lazydocker.tar.gz
+tar -xzf $TMPDIR/lazydocker.tar.gz lazydocker
+rm -f $TMPDIR/lazydocker.tar.gz
+
+mv lazydocker $HOME/.local/bin/lazydocker
+chmod +x $HOME/.local/bin/lazydocker
