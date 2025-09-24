e8f241739394cb7529c41941d6efbaadb838c3e3
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-09-24 21:50:42 +0100

    chore(install): create some more user folders

--- linux/install/base.sh
+++ linux/install/base.sh
@@ -4,10 +4,13 @@ set -e
 
 export PATH="$HOME/.local/bin:$PATH"
 
+mkdir -p $HOME/.cache
+mkdir -p $HOME/.config
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/etc
 mkdir -p $HOME/.local/lib
 mkdir -p $HOME/.local/lib64
+mkdir -p $HOME/.local/run
 mkdir -p $HOME/.local/share
 
 curl -sfSL https://go.ponces.dev/piu | bash
