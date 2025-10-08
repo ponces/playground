893f5050fe8b2e67fd4fbd0836ef643e606c6314
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-10-08 16:12:20 +0100

    refactor(run): simplify API calls

--- linux/run/gas.sh
+++ linux/run/gas.sh
@@ -1,6 +1,11 @@
-#!/system/bin/sh
+#!/bin/bash
 
 set -e
+
+url="https://precoscombustiveis.dgeg.gov.pt/api/PrecoComb/PesquisarPostos"
+mapsUrl="https://www.google.com/maps/search/?api=1&query="
+brand="29" # Galp
+gasTypes="2101,2105" # Diesel and Diesel+
 
 if [ ! -z "$1" ] && [ ! -z "$2" ]; then
     district="$1"
@@ -13,55 +18,28 @@ else
     city="Maia"
 fi
 
-url="https://precoscombustiveis.dgeg.gov.pt/api/PrecoComb/PesquisarPostos"
-mapsUrl="https://www.google.com/maps/search/?api=1&query="
-brand="29" # Galp
-regularDieselID="2101" # Regular Diesel
-premiumDieselID="2105" # Diesel+
-
-fetchPrices() {
-    curl -sfkSL "${url}?idsTiposComb=${1}&idMarca=${brand}&qtdPorPagina=999&pagina=1"
-}
-
-res_regular=$(fetchPrices "$regularDieselID")
-regular_data=$(echo "$res_regular" | jq -r --arg district "$district" --arg city "$city" '
-    .resultado 
-    | map(select(.Distrito == $district and .Municipio == $city))
-    | map({
-        key: (.Id | tostring),
-        value: {
-            "regularPrice": (.Preco | gsub(" €"; "") | gsub(","; ".") | tonumber),
-            "Nome": .Nome,
-            "Morada": .Morada,
-            "Latitude": .Latitude,
-            "Longitude": .Longitude
-        }
-    })
-    | from_entries
-')
-
-res_premium=$(fetchPrices "$premiumDieselID")
-premium_data=$(echo "$res_premium" | jq -r --arg district "$district" --arg city "$city" '
-    .resultado 
-    | map(select(.Distrito == $district and .Municipio == $city))
-    | map({
-        key: (.Id | tostring),
-        value: {
-            "premiumPrice": (.Preco | gsub(" €"; "") | gsub(","; ".") | tonumber)
-        }
-    })
-    | from_entries
-')
-
-if [ -z "$regular_data" ] || [ -z "$premium_data" ]; then
-    echo "ERROR: Could not fetch data for one or both fuel types."
+res=$(curl -sfkSL "${url}?idsTiposComb=${gasTypes}&idMarca=${brand}&qtdPorPagina=999&pagina=1")
+if [ -z "$res" ]; then
+    echo "ERROR: Could not fetch data from the API."
     exit 1
 fi
 
-echo "$regular_data" "$premium_data" | jq -r -s --arg mapsUrl "$mapsUrl" --arg city "$city" '
-    .[0] * .[1]
-    | to_entries 
-    | map(.value) 
-    | sort_by(.premiumPrice)[]
-    | "\nName:     \(.Nome)\nAddress:  \(.Morada)\nCity:     \($city)\nLocation: \($mapsUrl)\(.Latitude)%2C\(.Longitude)\nDiesel:   \(.regularPrice)€\nDiesel+:  \(.premiumPrice)€"
+echo "$res" | jq -r --arg district "$district" --arg city "$city" --arg mapsUrl "$mapsUrl" '
+    .resultado
+    | map(select(.Distrito == $district and .Municipio == $city))
+    | map(.Preco = (.Preco | gsub(" €"; "") | gsub(","; ".") | tonumber))
+    | group_by(.Id)
+    | map({
+        Id: (.[0].Id),
+        Name: (.[0].Nome),
+        Address: (.[0].Morada),
+        Latitude: (.[0].Latitude),
+        Longitude: (.[0].Longitude),
+        DieselPrice: ((map(select(.Combustivel == "Gasóleo simples")) | if length > 0 then .[0].Preco else null end)),
+        DieselPlusPrice: ((map(select(.Combustivel == "Gasóleo especial")) | if length > 0 then .[0].Preco else null end))
+    })
+    | map(select(.DieselPrice != null or .DieselPlusPrice != null))
+    | sort_by(.DieselPlusPrice // .DieselPrice)
+    | .[]
+    | "\nName:     \(.Name)\nAddress:  \(.Address)\nCity:     \($city)\nLocation: \($mapsUrl)\(.Latitude),\(.Longitude)\nDiesel:   \(.DieselPrice // "-")€\nDiesel+:  \(.DieselPlusPrice // "-")€"
 '
