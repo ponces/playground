ba6d98bcbf2285dc49b39e7ddc346f89eb207836
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-10-08 14:50:47 +0100

    refactor(run): display both regular and premium gas prices

--- linux/run/gas.sh
+++ linux/run/gas.sh
@@ -2,30 +2,66 @@
 
 set -e
 
+if [ ! -z "$1" ] && [ ! -z "$2" ]; then
+    district="$1"
+    city="$2"
+elif [ ! -z "$1" ]; then
+    district="Porto"
+    city="$1"
+else
+    district="Porto"
+    city="Maia"
+fi
+
 url="https://precoscombustiveis.dgeg.gov.pt/api/PrecoComb/PesquisarPostos"
 mapsUrl="https://www.google.com/maps/search/?api=1&query="
-gasType="2105" # Diesel+
 brand="29" # Galp
+regularDieselID="2101" # Regular Diesel
+premiumDieselID="2105" # Diesel+
 
-listPrices() {
-    res2=$(echo "$res" | jq -r --arg district "$1" --arg city "$2" --arg mapsUrl "$mapsUrl" '.resultado
-    | map(.Preco = (.Preco | gsub(" €"; "") | gsub(","; ".") | tonumber))
-    | map(select(.Distrito == $district and .Municipio == $city))
-    | group_by(.Municipio)
-    | .[] | sort_by(.Preco)[]
-    | "Name:     \(.Nome)\nAddress:  \(.Morada)\nCity:     \($city)\nLocation: \($mapsUrl)\(.Latitude),\(.Longitude)\nPrice:    \(.Preco)€\n"')
-    if [ ! -z "$res2" ]; then
-        echo "$res2"
-    else
-        echo "No results found for $1, $2"
-    fi
+fetchPrices() {
+    curl -sfkSL "${url}?idsTiposComb=${1}&idMarca=${brand}&qtdPorPagina=999&pagina=1"
 }
 
-res=$(curl -sfkSL "${url}?idsTiposComb=${gasType}&idMarca=${brand}&qtdPorPagina=999&pagina=1")
-if [ ! -z "$1" ] && [ ! -z "$2" ]; then
-    listPrices "$1" "$2"
-elif [ ! -z "$1" ]; then
-    listPrices "Porto" "$1"
-else
-    listPrices "Porto" "Maia"
+res_regular=$(fetchPrices "$regularDieselID")
+regular_data=$(echo "$res_regular" | jq -r --arg district "$district" --arg city "$city" '
+    .resultado 
+    | map(select(.Distrito == $district and .Municipio == $city))
+    | map({
+        key: (.Id | tostring),
+        value: {
+            "regularPrice": (.Preco | gsub(" €"; "") | gsub(","; ".") | tonumber),
+            "Nome": .Nome,
+            "Morada": .Morada,
+            "Latitude": .Latitude,
+            "Longitude": .Longitude
+        }
+    })
+    | from_entries
+')
+
+res_premium=$(fetchPrices "$premiumDieselID")
+premium_data=$(echo "$res_premium" | jq -r --arg district "$district" --arg city "$city" '
+    .resultado 
+    | map(select(.Distrito == $district and .Municipio == $city))
+    | map({
+        key: (.Id | tostring),
+        value: {
+            "premiumPrice": (.Preco | gsub(" €"; "") | gsub(","; ".") | tonumber)
+        }
+    })
+    | from_entries
+')
+
+if [ -z "$regular_data" ] || [ -z "$premium_data" ]; then
+    echo "ERROR: Could not fetch data for one or both fuel types."
+    exit 1
 fi
+
+echo "$regular_data" "$premium_data" | jq -r -s --arg mapsUrl "$mapsUrl" --arg city "$city" '
+    .[0] * .[1]
+    | to_entries 
+    | map(.value) 
+    | sort_by(.premiumPrice)[]
+    | "\nName:     \(.Nome)\nAddress:  \(.Morada)\nCity:     \($city)\nLocation: \($mapsUrl)\(.Latitude)%2C\(.Longitude)\nDiesel:   \(.regularPrice)€\nDiesel+:  \(.premiumPrice)€"
+'
