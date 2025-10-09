866bab38d1c33aa0bf3df27f1cdbbd4b0b230d68
Alberto Ponces <4687488+ponces@users.noreply.github.com>
2025-10-09 18:23:05 +0100

    fix(run): update `ponces` user create script

--- linux/run/ponces.sh
+++ linux/run/ponces.sh
@@ -4,6 +4,7 @@ set -e
 
 [ "$(id -u)" -ne 0 ] && SUDO="sudo" || SUDO=""
 
+id="1001"
 user="ponces"
 pass="ponces"
 
@@ -11,17 +12,17 @@ $SUDO apt-get update
 $SUDO apt-get install -y sudo
 
 if ! command -v useradd >/dev/null; then
-    $SUDO addgroup "$user"
-    $SUDO adduser "$user" --home "/home/$user" --gecos "$user" --ingroup "$user" --shell "/bin/bash"
+    $SUDO addgroup "$user" --gid "$id"
+    $SUDO adduser "$user" --home "/home/$user" --gecos "$user" --uid "$id" --ingroup "$user" --shell "/bin/bash"
 else
-    $SUDO groupadd "$user"
-    $SUDO useradd "$user" --create-home --home-dir "/home/$user" --gid "$user" --shell "/bin/bash"
+    $SUDO groupadd "$user" --gid "$id"
+    $SUDO useradd "$user" --create-home --home-dir "/home/$user" --uid "$id" --gid "$id" --shell "/bin/bash"
 fi
 
 echo $user:$pass | chpasswd
 
 $SUDO adduser "$user" sudo
 
-if [ -f /etc/wsl.conf ]; then
+if [ -f /etc/wsl.conf ] && ! grep -q "default" /etc/wsl.conf; then
     printf "[user]\ndefault = $user\n" | $SUDO tee -a /etc/wsl.conf
 fi
