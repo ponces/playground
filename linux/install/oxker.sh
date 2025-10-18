dfb04bd8ac89f144ce758184d1eecf2b32c2b631
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-10-18 23:26:27 +0100

    fix(install): properly handle `ARCH` and `OS` vars

--- linux/install/oxker.sh
+++ linux/install/oxker.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "$TMPDIR" ] && [ -d /tmp ] && TMPDIR="/tmp"
-[ "$(uname -m)" = "aarch64" ] && ARCH="aarch64" || ARCH="x86_64"
+ARCH="$(uname -m)"
 
 curl -sfSL "https://www.github.com/mrjackwills/oxker/releases/latest/download/oxker_linux_${ARCH}.tar.gz" -o $TMPDIR/oxker.tar.gz
 tar -xzf $TMPDIR/oxker.tar.gz oxker
--- linux/run/apkeep.sh
+++ linux/run/apkeep.sh
@@ -3,14 +3,15 @@
 set -e
 
 [ -z "$TMPDIR" ] && [ -d /tmp ] && TMPDIR="/tmp"
-[ "$(uname -m)" = "aarch64" ] && ARCH="aarch64-linux-android" || ARCH="x86_64-unknown-linux-gnu"
+ARCH="$(uname -m)"
+[ ! -z "$TERMUX_VERSION" ] && OS="linux-android" || OS="unknown-linux-gnu"
 
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <id> [<version>]"
     exit 1
 fi
 
-curl -sfSL "https://github.com/EFForg/apkeep/releases/latest/download/apkeep-${ARCH}" -o $TMPDIR/apkeep
+curl -sfSL "https://github.com/EFForg/apkeep/releases/latest/download/apkeep-${ARCH}-${OS}" -o $TMPDIR/apkeep
 chmod +x $TMPDIR/apkeep
 
 if [ ! -z "$2" ]; then
